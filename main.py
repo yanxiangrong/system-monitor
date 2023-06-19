ab94a2b3444c77d5b416b6478ed45fd0d91f8edf
“yanxiangrong“ <yanxiangrong_2@outlook.com>
2023-06-19 10:16:40 +0800

    ,

--- main.py
+++ main.py
@@ -1,145 +1,16 @@
-import time
-from datetime import datetime
-from typing import Iterable
+# 这是一个示例 Python 脚本。
 
-import clickhouse_connect
-import psutil
-
-from utils import *
+# 按 ⌃R 执行或将其替换为您的代码。
+# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
 
 
-def get_time():
-    now_time = datetime.now()
-    # print(f'time={now_time}')
-    return now_time
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
 
 
-def get_status():
-    s_cpu = psutil.cpu_percent(interval=None, percpu=True)
-    if not isinstance(s_cpu, Iterable):
-        s_cpu = [s_cpu]
-    cpu = [{
-        'core': i,
-        'percent': s_cpu[i]
-    } for i in range(psutil.cpu_count())]
-    # print(f'{cpu=}')
-
-    s_vmem = psutil.virtual_memory()
-    memory = {
-        'total': round_half_up(s_vmem.total / 1024 ** 3),
-        'used': round_half_up((s_vmem.total - s_vmem.available) / 1024 ** 3),
-        'percent': s_vmem.percent
-    }
-    # print(f'{memory=}')
-
-    s_swap = psutil.swap_memory()
-    swap = {
-        'total': round_half_up(s_swap.total / 1024 ** 3),
-        'used': round_half_up(s_swap.used / 1024 ** 3),
-        'percent': s_swap.percent
-    }
-    # print(f'{swap=}')
-
-    s_disk_part = psutil.disk_partitions()
-    disk_parts = [{
-        'device': p.device,
-        'mountpoint': p.mountpoint,
-    } for p in s_disk_part]
-    for p in disk_parts:
-        usage = psutil.disk_usage(p['mountpoint'])
-        p |= {
-            'total': round_half_up(usage.total / 1024 ** 3),
-            'used': round_half_up(usage.used / 1024 ** 3),
-            'percent': usage.percent
-        }
-    # print(f'{disk_parts=}')
-
-    s_disk_io = psutil.disk_io_counters(perdisk=True)
-    disk_io = [{
-        'device': k,
-        'read': round_half_up(s_disk_io[k].read_bytes / 1024 ** 3),
-        'write': round_half_up(s_disk_io[k].write_bytes / 1024 ** 3),
-    } for k in s_disk_io]
-    # print(f'{disk_io=}')
-
-    s_net_io = psutil.net_io_counters(pernic=True)
-    net_io = [{
-        'device': k,
-        'send': round_half_up(s_net_io[k].bytes_sent / 1024 ** 3),
-        'recv': round_half_up(s_net_io[k].bytes_recv / 1024 ** 3),
-    } for k in s_net_io if is_physical_if(k)]
-    # print(f'{net_io=}')
-
-    if psutil.MACOS and not is_root():
-        net_conn = None
-    else:
-        net_conn = {'count': len(psutil.net_connections())}
-
-    # print(f'{net_conn=}')
-
-    # print(psutil.sensors_temperatures())
-
-    processes = {'count': len(psutil.pids())}
-    # print(f'{processes=}')
-
-    # print()
-    return {
-        'cpu': cpu,
-        'memory': memory,
-        'swap': swap,
-        'disk_parts': disk_parts,
-        'disk_io': disk_io,
-        'net_io': net_io,
-        'net_conn': net_conn,
-        'processes': processes
-    }
-
-
-class App:
-    def __init__(self):
-        self.client = clickhouse_connect.get_client(host='8.134.13.200', username='default', database='monitor',
-                                                    password='world')
-
-    def save_machine_name(self):
-        self.client.insert('machine', [(machine_name(), machine_uuid())], ('name', 'uuid'))
-
-    def save(self, now_time: datetime, m_uuid: uuid.UUID, status: dict[str, dict | list]):
-        for key, value in status.items():
-            rows = []
-            keys = []
-            if isinstance(value, list) and len(value) > 0:
-                keys = ['time', 'uuid'] + list(value[0].keys())
-                for row in value:
-                    rows.append([now_time, m_uuid] + list(row.values()))
-            elif isinstance(value, dict):
-                keys = ['time', 'uuid'] + list(value.keys())
-                rows.append([now_time, machine_uuid()] + list(value.values()))
-
-            if len(rows) > 0 and len(keys) > 0:
-                self.client.insert(key, rows, keys)
-
-    def run(self):
-        self.save_machine_name()
-        m_uuid = machine_uuid()
-
-        while True:
-            now_time = get_time()
-            res = get_status()
-            # print(res)
-            print(now_time)
-
-            self.save(now_time, m_uuid, res)
-            time.sleep(1.5)
-
-
-def main():
-    while True:
-        try:
-            App().run()
-        except Exception as exp:
-            print(exp)
-        time.sleep(10)
-
-
+# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    main()
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
